4be2db172cd87899ac45211a3d806da1d8a0d5ac
Nowshin Tisha <nafisatisha230@gmail.com>
2026-09-08 17:47:47 +0600

    feat(home): assemble homepage from hero, categories, featured campaigns, how-it-works and CTA

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import Hero from "@/components/home/Hero";
+import CategoryGrid from "@/components/home/CategoryGrid";
+import FeaturedCampaigns from "@/components/home/FeaturedCampaigns";
+import HowItWorks from "@/components/home/HowItWorks";
+import CTASection from "@/components/home/CTASection";
 
-
-export default function Home() {
+export default function HomePage() {
   return (
-    <div className="">
-    
-    </div>
+    <>
+      <Hero />
+      <CategoryGrid />
+      <FeaturedCampaigns />
+      <HowItWorks />
+      <CTASection />
+    </>
   );
 }
